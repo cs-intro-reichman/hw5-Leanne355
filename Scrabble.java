5ca4c50999b8c7c7781100b304ce7238bde1d9cb
Leanne355 <Leanne.sharoni@post.runi.ac.il>
2024-12-12 08:54:31 +0200

    finished hw

--- Scrabble.java
+++ Scrabble.java
@@ -121,7 +121,6 @@ public class Scrabble {
 	        System.out.println("Ran out of letters. Total score: " + score + " points");
 		} else {
 			System.out.println("End of hand. Total score: " + score + " points");
-			System.out.println();
 		}
 	}
 
