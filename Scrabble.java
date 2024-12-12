db66fc721195632c49339573c82e41773d8415dc
Leanne355 <Leanne.sharoni@post.runi.ac.il>
2024-12-12 08:49:36 +0200

    finished hw

--- Scrabble.java
+++ Scrabble.java
@@ -108,7 +108,6 @@ public class Scrabble {
 				if (isWordInDictionary(input)){
 					score += wordScore(input);
 					System.out.println(input + " earned " + wordScore(input) + " points. " + "Score: " + score + " points");
-					System.out.println();
 					hand = MyString.remove(hand, input);
 				} else {
 				    System.out.println("Invalid word. Try again.");
